3a5c8df2a2021338ef38051f7d6396496f469119
Parthtate <parthtate575@gmail.com>
2025-04-26 18:36:01 +0530

    fix main urls.py

--- tweet/urls.py
+++ tweet/urls.py
@@ -4,10 +4,10 @@ from . import views
 app_name = 'tweet' 
 
 urlpatterns = [
-    path('tweet/', views.all_tweet, name='all_tweet' ),
-    path('tweet/create/', views.tweet_create, name='tweet_create' ),
-    path('tweet/<int:tweet_id>/edit/', views.tweet_edit, name='tweet_edit' ),
-    path('tweet/<int:tweet_id>/delete/', views.tweet_delete, name='tweet_delete' ),
+    path('', views.all_tweet, name='all_tweet' ),
+    path('create/', views.tweet_create, name='tweet_create' ),
+    path('<int:tweet_id>/edit/', views.tweet_edit, name='tweet_edit' ),
+    path('<int:tweet_id>/delete/', views.tweet_delete, name='tweet_delete' ),
     path('register/', views.register, name='register' ),
     
 
