bb2a778ef78d3c51e7bbdfb6c42c3122d48c5524
Parthtate <parthtate575@gmail.com>
2025-04-26 12:57:14 +0530

    fix csrf bug

--- tweetCenter/settings.py
+++ tweetCenter/settings.py
@@ -32,6 +32,11 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['tweetcenter.onrender.com', 'localhost', '127.0.0.1']
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://tweetcenter.onrender.com',
+]
+
+
 
 # Application definition
 
